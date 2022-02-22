c137b1eb0b52ba9384d6a9930d61087993c78ce0
Rammah Al Shawaf <shawaf.ramy@gmail.com>
2022-02-22 12:48:32 +0100

    Added New, Create, Show and Destroy to reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,31 @@
 class ReservationsController < ApplicationController
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+  def new
+    @creature = Creature.find(params[:creature_id])
+    @reservation = Reservation.new
+  end
+  def create
+    @creature = Creature.find(params[:creature_id])
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+    @reservation.creature_id = @creature.id
+    if @reservation.save
+      redirect_to creature_path(@creature)
+    else
+      render :new
+    end
+  end
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.delete
+    redirect_to creature_path(@reservation.creature)
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:starting_date, :ending_date)
+  end
 end
